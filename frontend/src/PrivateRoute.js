b4b16ccbf48268042b256114abed7f4f5b922681
sufyanhr <sofy.fsh@gmail.com>
2025-11-14 10:40:14 +0200

    Remove old frontend for clean initial release

--- frontend/src/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-
-export default function PrivateRoute({ children }) {
-  const token = localStorage.getItem('token')
-  // إذا لم يوجد توكن يرجعك إلى صفحة تسجيل الدخول
-  return token ? children : <Navigate to="/login" replace />
-}
--- frontend/src/api/dashboard.js
+++
@@ -1,6 +0,0 @@
-import api from './axios'
-
-export const getDashboardSummary = async () => {
-  const response = await api.get('/dashboard/summary')
-  return response.data
-}
