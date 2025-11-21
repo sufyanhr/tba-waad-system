2d52fa645f306630b353236c2553e7d390dbc774
sufyanhr <106785661+sufyanhr@users.noreply.github.com>
2025-11-21 19:47:48 +0200

    Delete frontend/src directory

--- frontend/src/layout/Pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-// ==============================|| LAYOUT - BLANK PAGES ||============================== //
-
-export default function PagesLayout() {
-  return <Outlet />;
-}
